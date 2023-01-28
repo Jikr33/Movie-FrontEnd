0997feac2ad10ec369615b79397073630993dc32
Jikr33 <54263616+Jikr33@users.noreply.github.com>
2023-01-28 16:37:45 +0800

    ss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 // import { Redirect, Switch } from "react";
 // import Home from "./home";
 // import Result from "./result";
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 ReactDOM.render(
     <React.StrictMode>
         <App />
