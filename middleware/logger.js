c288f027546a64c5a3cd0acf3e7f1999528f62bb
Jikr33 <54263616+Jikr33@users.noreply.github.com>
2023-01-28 16:29:45 +0800

    s

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-const logger = (req, res, next) => {
-    req.userID = "killer";
-    console.log(
-        `${req.method} ${req.protocol}://${req.hostname}${req.originalUrl}`
-    );
-    next();
-};
-
-module.exports = logger;
